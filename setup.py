4df23c9f0e4be45a4a053cb290d2533516cb0e85
Tobias Brox <tobias@redpill-linpro.com>
2017-08-30 00:31:41 +0200

    fix proper command name, fix dependency, compatibility fix on version number, modified defaults to fit with my workflow - due dates should expire immediately by default so we can set a proper due date

--- setup.py
+++ setup.py
@@ -37,10 +37,10 @@ setup(
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
     ],
-    scripts=['calendar-cli.py'],
+    scripts=['calendar-cli.py', 'calendar-cli'],
     install_requires=[
         'icalendar',
-        'caldav>=0.4.0.dev',
+        'caldav>=0.5.0',
         'pytz',
         'tzlocal'
     ],
