02642715c88eb10cab16153129d653d546d1a502
Tobias Brox <tobias@redpill-linpro.com>
2019-09-12 08:26:39 +0000

    python3 compatibility fix

--- calendar-cli.py
+++ calendar-cli.py
@@ -162,6 +162,9 @@ def _calendar_addics(caldav_conn, ics, uid, args):
 
     try:
         c = find_calendar(caldav_conn, args)
+        ## unicode strings vs byte strings is a minefield in python3 ... so, re.search demands a string here ...
+        if hasattr(ics, 'decode'):
+            ics = ics.decode('utf-8')
         if re.search(r'^METHOD:[A-Z]+[\r\n]+',ics,flags=re.MULTILINE) and args.ignoremethod:
             ics = re.sub(r'^METHOD:[A-Z]+[\r\n]+', '', ics, flags=re.MULTILINE)
             print ("METHOD property found and ignored")
