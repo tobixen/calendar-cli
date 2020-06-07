aa13a2c65888a79fc40848fa19efe194feb065fb
Tobias Brox <tobias@redpill-linpro.com>
2020-06-07 11:20:50 +0200

    Hm, the --default-due does not really do what it said in the --help

--- calendar-cli.py
+++ calendar-cli.py
@@ -874,8 +874,8 @@ def main():
     todo_add_parser.set_defaults(func=todo_add)
 
     todo_list_parser = todo_subparsers.add_parser('list')
-    todo_list_parser.add_argument('--todo-template', help="Template for printing out the event", default="{dtstart}{dtstart_passed_mark} {due}{due_passed_mark} {summary}")
-    todo_list_parser.add_argument('--default-due', help="Default number of days from a task is submitted until it's considered due", type=int, default=14)
+    todo_list_parser.add_argument('--todo-template', help="Template for printing out the event", default="{dtstart}{dtstart_passedmark} {due}{due_passed_mark} {summary}")
+    todo_list_parser.add_argument('--default-due', help="If a task has no due date set, list it with the due date set N days from today", type=int, default=14)
     todo_list_parser.add_argument('--list-categories', help="Instead of listing the todo-items, list the unique categories used", action='store_true')
     todo_list_parser.add_argument('--timestamp-format', help="strftime-style format string for the output timestamps", default="%Y-%m-%d (%a)")
     todo_list_parser.set_defaults(func=todo_list)
