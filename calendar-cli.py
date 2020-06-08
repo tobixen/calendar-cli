13c2616de265ead2ae6be6359f9f656a1474eeb6
Tobias Brox <tobias@redpill-linpro.com>
2020-06-08 15:01:37 +0200

    the whole docstring is written out in the help-message, without line breaks, and it gets quite ugly.  Perhaps this will be better?

--- calendar-cli.py
+++ calendar-cli.py
@@ -1,22 +1,11 @@
 #!/usr/bin/env python
 
 """
-https://github.com/tobixen/calendar-cli/ - high-level cli against caldav servers
+https://github.com/tobixen/calendar-cli/ - high-level cli against caldav servers.
 
-Copyright (C) 2013-2020 Tobias Brox and other contributors
+Copyright (C) 2013-2020 Tobias Brox and other contributors.
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+See https://www.gnu.org/licenses/gpl-3.0.en.html for license information.
 """
 
 import argparse
